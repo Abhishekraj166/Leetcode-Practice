cb7a6aefcbe5e659763e5d673ccfb23abe350f11
Abhishekraj166 <abhishek.raj166@gmail.com>
2020-04-06 23:14:52 -0500

    Move zeroes leetcode practice

---
+++ move zeroes.cpp
@@ -0,0 +1,21 @@
+#include<vector>
+#include<math.h>
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums){ 
+        int i = 0;
+    int j = 0;
+ 
+    while (j < nums.size()) {
+        if (nums[j] != 0) {
+            nums[i++] = nums[j];
+        }
+ 
+        j++;
+    }
+ 
+        while (i < nums.size()) {
+        nums[i++] = 0;
+    }
+    }
+};
